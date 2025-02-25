25a91d3284334c32c2fd75123685bc9efed4ca1e
Ngọc Thạch <ngocthach.2004bp@gmail.com>
2025-02-25 21:59:14 +0700

    update comment code

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ app.controller('ProductController', function($scope, $http) {
     $scope.products = [];
     $scope.newProduct = {};
 
-    // Load danh sách sản phẩm từ API
     function loadProducts() {
-        $http.get('http://localhost:8000/api/products') // Thay đổi đường dẫn nếu cần
+        $http.get('http://localhost:8000/api/products')
             .then(function(response) {
                 $scope.products = response.data;
             })
@@ -15,15 +14,13 @@ app.controller('ProductController', function($scope, $http) {
             });
     }
 
-    // Gọi hàm để load sản phẩm ngay khi trang tải
     loadProducts();
 
-    // Thêm sản phẩm mới
     $scope.addProduct = function() {
         $http.post('http://localhost:8000/api/products', $scope.newProduct)
             .then(function(response) {
-                $scope.products.push(response.data); // Thêm sản phẩm vào danh sách hiển thị
-                $scope.newProduct = {}; // Reset form
+                $scope.products.push(response.data);
+                $scope.newProduct = {};
             })
             .catch(function(error) {
                 console.error("Lỗi khi thêm sản phẩm:", error);
